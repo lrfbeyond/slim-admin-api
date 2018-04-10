c97b61ca7003581fda60d0fa871443a3e69298f2
liurenfei <13966913@qq.com>
2018-04-10 14:56:26 +0800

    通用函数库

---
+++ app/function.php
@@ -0,0 +1,68 @@
+<?php 
+
+//数据签名认证
+function dataAuthSign($data){
+    //数据类型检测
+    if(!is_array($data)){
+        $data = (array)$data;
+    }
+    ksort($data); //排序
+    $code = http_build_query($data); //url编码并生成query字符串
+    $sign = sha1($code); //生成签名
+    return $sign;
+}
+
+
+/**
+ * 获取客户端IP地址
+ * @param integer   $type 返回类型 0 返回IP地址 1 返回IPV4地址数字
+ * @param boolean   $adv 是否进行高级模式获取（有可能被伪装）
+ * @return mixed
+ */
+function getip($type = 0, $adv = false){
+    $type      = $type ? 1 : 0;
+    $ip = null;
+    if (null !== $ip) {
+        return $ip[$type];
+    }
+
+    if ($adv) {
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            $pos = array_search('unknown', $arr);
+            if (false !== $pos) {
+                unset($arr[$pos]);
+            }
+            $ip = trim(current($arr));
+        } elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+    } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    // IP地址合法验证
+    $long = sprintf("%u", ip2long($ip));
+    $ip   = $long ? [$ip, $long] : ['0.0.0.0', 0];
+    return $ip[$type];
+}
+
+/**
+ * 检测是否使用手机访问
+ * @access public
+ * @return bool
+ */
+function isMobile(){
+    if (isset($_SERVER['HTTP_VIA']) && stristr($_SERVER['HTTP_VIA'], "wap")) {
+        return true;
+    } elseif (isset($_SERVER['HTTP_ACCEPT']) && strpos(strtoupper($_SERVER['HTTP_ACCEPT']), "VND.WAP.WML")) {
+        return true;
+    } elseif (isset($_SERVER['HTTP_X_WAP_PROFILE']) || isset($_SERVER['HTTP_PROFILE'])) {
+        return true;
+    } elseif (isset($_SERVER['HTTP_USER_AGENT']) && preg_match('/(blackberry|configuration\/cldc|hp |hp-|htc |htc_|htc-|iemobile|kindle|midp|mmp|motorola|mobile|nokia|opera mini|opera |Googlebot-Mobile|YahooSeeker\/M1A1-R2D2|android|iphone|ipod|mobi|palm|palmos|pocket|portalmmm|ppc;|smartphone|sonyericsson|sqh|spv|symbian|treo|up.browser|up.link|vodafone|windows ce|xda |xda_)/i', $_SERVER['HTTP_USER_AGENT'])) {
+        return true;
+    } else {
+        return false;
+    }
+}
