e562de008666e43727ef72b9be850d2c18e5d433
liurenfei <13966913@qq.com>
2018-04-10 16:19:01 +0800

    鉴权中间件

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -14,18 +14,36 @@ class AuthMiddleware
     public function __invoke($request, $response, $next)
     {
         //$response->getBody()->write('BEFORE');
-        //print_r($_SESSION['admin_auth']);
-        if (!isset($_SESSION['admin_auth'])) {
+        $hasLogin = $this->chkSign();
+        if (false === $hasLogin) {
             $data = [
                 'code' => 101,
                 'msg' => '未登录'
             ];
             return $response->withJson($data);
         }
+        
         $response = $next($request, $response);
         //$response->getBody()->write('AFTER');
 
         return $response;
+    }
+
+    private function chkSign()
+    {
+        $auth = isset($_SESSION['admin_auth']) ? $_SESSION['admin_auth'] : '';
+        $token = isset($_SERVER['HTTP_X_TOKEN']) ? $_SERVER['HTTP_X_TOKEN'] : '';
+        if (!empty($auth) && !empty($token)) {
+            $key = $this->safekey; //安全密钥
+            $sign = dataAuthSign($auth, $key);
+            if ($sign == $token) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 }
 
